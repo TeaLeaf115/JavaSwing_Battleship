c0fbd7f5f9e6a02dd1a31bb4f33f031d9ba4a384
TeaLeaf115 <91578545+TeaLeaf115@users.noreply.github.com>
2023-11-16 22:52:20 +0000

    Added getters and setter for the turn order.

--- src/gameLogic/GameState.java
+++ src/gameLogic/GameState.java
@@ -6,7 +6,23 @@ public class GameState {
     private final int COMPUTER_TURN = 1;
 
     public GameState() {
-
+		turnOrder = 0;
     }
+
+	public void nextTurn() {
+		turnOrder++;
+	}
+
+	public int getTurn() {
+		return turnOrder % 2 == 0 ? PLAYER_TURN : COMPUTER_TURN;
+	}
+
+	public boolean isPlayerTurn() {
+		return turnOrder % 2 == 0;
+	}
+
+	public boolean isComputerTurn() {
+		return turnOrder % 2 == 1;
+	}
 
 }
